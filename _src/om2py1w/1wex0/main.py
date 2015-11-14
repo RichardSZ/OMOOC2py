b4a3d86fa8ba36a35a0e3fac7590011273729f32
RichardSZ <RichardSZ@foxmail.com>
2015-11-14 09:50:30 +0800

    remove input finename line

--- _src/om2py1w/1wex0/main.py
+++ _src/om2py1w/1wex0/main.py
@@ -1,15 +1,16 @@
-prompt=">"
+﻿prompt=">"
 
-filename=raw_input(prompt)
+#filename=raw_input(prompt)
+message = raw_input(prompt)
 
-Mydairy=open(filename)
-print("Historic dairy:" )
+Mydairy=open('mydairy.txt','r')
+print("dairy log:" )
 print Mydairy.read()
 print "\n"
 
 print"Dairy now..."
 
-targetline = raw_input(">>>") 
-Mydairy=open(filename,'a+')
+target = raw_input(">>>") 
+Mydairy=open('mydairy.txt','a+')
 Mydairy.write("\n")
-Mydairy.write(targetline)
+Mydairy.write(target)
